7de0a0ddc58a771838de59464a45310d8f5a199e
Kubokoo <Kubokoo@users.noreply.github.com>
2021-01-08 01:46:27 +0100

    Dodanie plików dla układu okna oraz pliku stylów

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,18 +1,22 @@
 package client;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getClassLoader().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader
+					.load(getClass()
+					.getResource("ui/MainWindow.fxml"));
+			Scene scene = new Scene(root, 300, 275);
+		    
+			primaryStage.setTitle("FXML Welcome");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
